21f6231f099f571f361cdc65fb85aee4b16b8d0c
aaggarw99 <aaggarw99@gmail.com>
2019-07-11 15:01:15 -0500

    teascript

--- teascript.py
+++ teascript.py
@@ -1,32 +1,7 @@
 from numpy.random import choice
 
-people = ["h.abneymcp@gmail.com",
-"aaggarw99@gmail.com",
-"atlas99@uchicago.edu",
-"wbrunner@uchicago.edu",
-"nikhitachakravarthy@gmail.com",
-"dustin.dienhart@gmail.com",
-"tsg44@cornell.edu",
-"medhagoyal@uchicago.edu",
-"margotaherman@uchicago.edu",
-"chrisjacin10@gmail.com",
-"sydney8jenkins@gmail.com",
-"tedkim97@uchicago.edu",
-"maxineking@uchicago.edu",
-"koenig1@uchicago.edu",
-"akoripelly@gmail.com",
-"jarvislam1999@uchicago.edu",
-"nlingareddy@uchicago.edu",
-"oliviamorkved@uchicago.edu",
-"nisenoff@uchicago.edu",
-"jaramirez28@cps.edu",
-"aschwartz@ucls.uchicago.edu",
-"etruelove@uchicago.edu",
-"eva@tuecke.com",
-"aprilwang@uchicago.edu",
-"pengwei@uchicago.edu",
-"Rubywerman@berkeley.edu",
-"rewong03@gmail.com"]
+names_file = open('names.txt', 'r')
+people = names_file.read().split(',\n')
 
 draw = choice(people, 2)
-print(draw)
+print(*draw, sep=', ')
